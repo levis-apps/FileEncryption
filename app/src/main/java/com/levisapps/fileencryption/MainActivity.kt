aa68ce21f42f25f80866ae3fd5bf94d5d1a82ecb
levis-apps <levisappss@gmail.com>
2020-09-27 20:21:49 +0300

    File Encryption 1.0

--- app/src/main/java/com/levisapps/fileencryption/MainActivity.kt
+++ app/src/main/java/com/levisapps/fileencryption/MainActivity.kt
@@ -12,6 +12,8 @@ import android.view.MenuItem
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -19,7 +21,6 @@ import androidx.biometric.BiometricPrompt
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.security.crypto.EncryptedFile
-import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
 import com.nbsp.materialfilepicker.MaterialFilePicker
 import com.nbsp.materialfilepicker.ui.FilePickerActivity
@@ -28,14 +29,10 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.nio.charset.StandardCharsets
 import java.util.regex.Pattern
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
 
-class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogListener,
-    PasswordDialog.PasswordDialogListener {
+class MainActivity : AppCompatActivity() {
+
     private var filepath = ""
-    //private var mUseBio = true
-    //private var firstFile = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,17 +41,6 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
         toolbar.showOverflowMenu()
 
         checkPermissions()
-
-        /*val sharedPreferences = EncryptedSharedPreferences.create(
-            "ID",
-            masterKeyAlias,
-            this,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-
-        mUseBio = sharedPreferences.getBoolean("use_biometrics", true)
-        firstFile = sharedPreferences.getInt("first_file", 1)*/
 
         save.setOnClickListener {
             checkPermissions()
@@ -171,26 +157,11 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
     fun newFile(view: View) {
         newFile.visibility = GONE
         newFileLayout.visibility = VISIBLE
-        /*if (firstFile == 1) {
-            val sharedPreferences = EncryptedSharedPreferences.create(
-                "ID",
-                masterKeyAlias,
-                this,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-            )
-            sharedPreferences.edit().putInt("first_file", ++firstFile).apply()
-            openNewPasswordDialog()
-        }*/
     }
 
     fun pickFile(view: View) {
         closeNewFileUI()
         MaterialFilePicker()
-            // Pass a source of context. Can be:
-            //    .withActivity(Activity activity)
-            //    .withFragment(Fragment fragment)
-            //    .withSupportFragment(androidx.fragment.app.Fragment fragment)
             .withActivity(this)
             // With cross icon on the right side of toolbar for closing picker straight away
             .withCloseMenu(true)
@@ -217,52 +188,112 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
                 FILE_PICKER_REQUEST_CODE -> {
                     filepath = data!!.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)!!
                     bioID()
-                    /*if (mUseBio) {
-                            bioID()
+                }
+                FILE_TO_ENC_PICKER_REQUEST_CODE -> {
+                    val alertDialog = AlertDialog.Builder(this)
+                    alertDialog.setTitle("Enter Password")
+                    val input = EditText(this)
+                    val lp = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                    )
+                    input.layoutParams = lp
+                    alertDialog.setView(input)
+
+                    alertDialog.setPositiveButton("encrypt") { _, _ ->
+                        val password = input.text.toString()
+
+                        if (password == "") {
+                            Toast.makeText(
+                                this,
+                                "Password is empty, cancelling...",
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            return@setPositiveButton
+                        }
+
+                        val fileName = data!!.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)!!
+                        var counter = 1
+                        val extension = getFileExtension(fileName)
+                        var name = "EncryptedFile$counter.$extension"
+                        val path =
+                            Environment.getExternalStorageDirectory()
+                                .toString() + "/File Encryption"
+                        var file = File(path, name)
+                        while (file.exists()) {
+                            name = "EncryptedFile${++counter}.$extension"
+                            file = File(path, name)
+                        }
+
+                        file.createNewFile()
+                        val encryption = FileEncryption()
+                        encryption.encryptFile(fileName, "$path/$name", password)
+                        Toast.makeText(
+                            this,
+                            "Encrypted image saved to $path with this name: $name",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    alertDialog.show()
+                }
+                ENC_FILE_PICKER_REQUEST_CODE -> {
+                    val alertDialog = AlertDialog.Builder(this)
+                    alertDialog.setTitle("Enter Password")
+                    val input = EditText(this)
+                    val lp = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                    )
+                    input.layoutParams = lp
+                    alertDialog.setView(input)
+
+                    alertDialog.setPositiveButton("decrypt") { _, _ ->
+                        val password = input.text.toString()
+
+                        if (password == "") {
+                            Toast.makeText(
+                                this,
+                                "Password is empty, cancelling...",
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            return@setPositiveButton
+                        }
+
+                        val fileName = data!!.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)!!
+                        val extension = getFileExtension(fileName)
+                        var counter = 1
+                        var name = "DecryptedFile$counter.$extension"
+                        val path =
+                            Environment.getExternalStorageDirectory()
+                                .toString() + "/File Encryption"
+                        var file = File(path, name)
+                        val file1 = File(fileName)
+                        while (file.exists()) {
+                            name = "DecryptedFile${++counter}.$extension"
+                            file = File(path, name)
+                        }
+
+                        file.createNewFile()
+                        val decryption = FileEncryption()
+                        val success = decryption.decryptFile(fileName, "$path/$name", password)
+                        if (success) {
+                            file1.delete()
+                            Toast.makeText(
+                                this,
+                                "Decrypted image saved to $path with this name: $name",
+                                Toast.LENGTH_LONG
+                            ).show()
                         } else {
-                            //openPasswordDialog()
-                        }*/
-
-                }
-                IMAGE_PICKER_REQUEST_CODE -> {
-                    val key = if (sharedPreferences.contains("key")) {
-                        sharedPreferences.getString("key", null)
-                    } else {
-                        keyGenerator()
+                            Toast.makeText(
+                                this,
+                                "Wrong password!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
                     }
-
-                    val imagepath = data!!.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)!!
-                    var counter = 1
-                    var name = "EncryptedImage$counter"
-                    val path = Environment.getExternalStorageDirectory().toString() + "/File Encryption"
-                    var file = File(path, name)
-                    while (file.exists()) {
-                        name = "EncryptedFile${++counter}.jpg"
-                        file = File(path, name)
-                    }
-
-                    val encryption = Encryption()
-                    encryption.encrypt(key!!, File(imagepath), file)
-                    Toast.makeText(this, "Encrypted image saved to $path with this name: $name", Toast.LENGTH_LONG).show()
-
-                }
-                ENC_IMAGE_PICKER_REQUEST_CODE -> {
-                    val key = sharedPreferences.getString("key", null)
-                    val imagepath = data!!.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)!!
-                    var counter = 1
-                    var name = "DecryptedImage$counter"
-                    val path = Environment.getExternalStorageDirectory().toString() + "/File Encryption"
-                    var file = File(path, name)
-                    val file1 = File(imagepath)
-                    while (file.exists()) {
-                        name = "DecryptedFile${++counter}.jpg"
-                        file = File(path, name)
-                    }
-
-                    val encryption = Encryption()
-                    encryption.decrypt(key!!, file1, file)
-                    file1.delete()
-                    Toast.makeText(this, "Decrypted image saved to $path with this name: $name", Toast.LENGTH_LONG).show()
+                    alertDialog.show()
                 }
             }
         }
@@ -282,7 +313,6 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
                         "Authentication error: $errString", Toast.LENGTH_SHORT
                     )
                         .show()
-                    //openPasswordDialog()
                 }
 
                 override fun onAuthenticationSucceeded(
@@ -300,7 +330,6 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
                         Toast.LENGTH_SHORT
                     )
                         .show()
-                    //openPasswordDialog()
                 }
             })
 
@@ -313,34 +342,12 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
         biometricPrompt.authenticate(promptInfo)
     }
 
-    /* private fun openNewPasswordDialog() {
-         val newPassDialog = NewPasswordDialog()
-         newPassDialog.show(supportFragmentManager, "new password dialog")
-     }
-
-     private fun openPasswordDialog() {
-         val newPassDialog = PasswordDialog()
-         newPassDialog.show(supportFragmentManager, "password dialog")
-     }*/
-
     private fun closeNewFileUI() {
         fname.setText("")
         fcontent.setText("")
         newFileLayout.visibility = GONE
         newFile.visibility = VISIBLE
     }
-
-    /*fun exportKey(view: View) {
-        AlertDialog.Builder(this@MainActivity)
-            .setTitle("Are you sure you want to export the encryption key to a file?")
-            .setMessage("The key will be written in plain text to a .txt file.\nYou should not keep that file on this device!")
-            .setPositiveButton("yes") { _, _ ->
-                val fileName = Environment.getExternalStorageDirectory().toString() + "/File Encryption/AES256_Key.txt"
-                File(fileName).writeText(masterKeyAlias)
-            }
-            .setNegativeButton("no") { _, _ -> }
-            .show()
-    }*/
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
@@ -371,10 +378,6 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
     fun pickImage(view: View) {
         closeNewFileUI()
         MaterialFilePicker()
-            // Pass a source of context. Can be:
-            //    .withActivity(Activity activity)
-            //    .withFragment(Fragment fragment)
-            //    .withSupportFragment(androidx.fragment.app.Fragment fragment)
             .withActivity(this)
             // With cross icon on the right side of toolbar for closing picker straight away
             .withCloseMenu(true)
@@ -384,22 +387,16 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
             .withRootPath(Environment.getExternalStorageDirectory().toString())
             // Showing hidden files
             .withHiddenFiles(true)
-            // Want to choose only jpg images
-            .withFilter(Pattern.compile(".*\\.(jpg|jpeg)$"))
             // Don't apply filter to directories names
             .withFilterDirectories(false)
-            .withTitle("Pick an image")
-            .withRequestCode(IMAGE_PICKER_REQUEST_CODE)
+            .withTitle("Pick a file")
+            .withRequestCode(FILE_TO_ENC_PICKER_REQUEST_CODE)
             .start()
     }
 
     fun pickEncImage(view: View) {
         closeNewFileUI()
         MaterialFilePicker()
-            // Pass a source of context. Can be:
-            //    .withActivity(Activity activity)
-            //    .withFragment(Fragment fragment)
-            //    .withSupportFragment(androidx.fragment.app.Fragment fragment)
             .withActivity(this)
             // With cross icon on the right side of toolbar for closing picker straight away
             .withCloseMenu(true)
@@ -409,66 +406,24 @@ class MainActivity : AppCompatActivity(), NewPasswordDialog.NewPasswordDialogLis
             .withRootPath(Environment.getExternalStorageDirectory().toString() + "/File Encryption")
             // Showing hidden files
             .withHiddenFiles(true)
-            // Want to choose only jpg images
-            .withFilter(Pattern.compile(".*\\.(jpg|jpeg)$"))
             // Don't apply filter to directories names
             .withFilterDirectories(false)
-            .withTitle("Pick an encrypted image")
-            .withRequestCode(ENC_IMAGE_PICKER_REQUEST_CODE)
+            .withTitle("Pick an encrypted file")
+            .withRequestCode(ENC_FILE_PICKER_REQUEST_CODE)
             .start()
     }
 
-    private fun keyGenerator() :String {
-        val path = Environment.getExternalStorageDirectory().toString() + "/File Encryption/" + "AESKey_for_images.txt"
-        val file = File(path)
-
-        val keyGen = KeyGenerator.getInstance("AES")
-        keyGen.init(256)
-        val key = keyGen.generateKey()
-        file.writeText(key.toString())
-        Toast.makeText(this, "AES key for images has been exported to a file and should be saved somewhere else or deleted. Path: $path", Toast.LENGTH_LONG).show()
-
-        val sharedPreferences = getSharedPreferences("sharedprefs", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("key", key.toString())
-        editor.apply()
-
-        return key.toString()
-    }
-
-    override fun applyNewPassword(password: String, useBiometrics: Boolean) {
-        val sharedPreferences = EncryptedSharedPreferences.create(
-            "ID",
-            masterKeyAlias,
-            this,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-
-        sharedPreferences
-            .edit()
-            .putString("users_password", password)
-            .putBoolean("use_biometrics", useBiometrics)
-            .apply()
-        //mUseBio = useBiometrics
-    }
-
-    override fun applyPassword(password: String): Boolean {
-        val sharedPreferences = EncryptedSharedPreferences.create(
-            "ID",
-            masterKeyAlias,
-            this,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-
-        return sharedPreferences.getString("users_password", null) == password
+    private fun getFileExtension(fullName: String?): String? {
+        checkNotNull(fullName)
+        val fileName = File(fullName).name
+        val dotIndex = fileName.lastIndexOf('.')
+        return if (dotIndex == -1) "" else fileName.substring(dotIndex + 1)
     }
 
     companion object {
         val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
         const val FILE_PICKER_REQUEST_CODE = 42
-        const val IMAGE_PICKER_REQUEST_CODE = 43
-        const val ENC_IMAGE_PICKER_REQUEST_CODE = 44
+        const val FILE_TO_ENC_PICKER_REQUEST_CODE = 43
+        const val ENC_FILE_PICKER_REQUEST_CODE = 44
     }
 }
